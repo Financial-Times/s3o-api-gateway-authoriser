cf8681dbb12b2aa3c477eae128578652fa96a658
ola.okejimi <ola.okejimi@ft.com>
2016-09-27 09:39:34 +0100

    Add unit test

--- authoriser.py
+++ authoriser.py
@@ -1,6 +1,6 @@
 from __future__ import print_function
 
-import re, urllib2
+import re, urllib2,boto3
 
 
 def lambda_handler(event, context):
@@ -12,18 +12,25 @@ def lambda_handler(event, context):
     apiGatewayArnTmp = tmp[5].split('/')
     awsAccountId = tmp[4]
 
+    restApiId = apiGatewayArnTmp[0]
+    stage = apiGatewayArnTmp[1]
+    ApiKeyId = restApiId + '-' + stage
+
     policy = AuthPolicy(principalId, awsAccountId)
-    policy.restApiId = apiGatewayArnTmp[0]
+    policy.restApiId = restApiId
     policy.region = tmp[3]
-    policy.stage = apiGatewayArnTmp[1]
+    policy.stage = stage
 
     try:
-        if auth_with_s3o(token):
+        if auth_with_s3o(token) or lookupKeyInDynamo(ApiKeyId, token):
             policy.allowAllMethods()
         else:
             policy.denyAllMethods()
     except:
-        policy.denyAllMethods()
+        if lookupKeyInDynamo(ApiKeyId, token):
+            policy.allowAllMethods()
+        else:
+            policy.denyAllMethods()
 
     return policy.build()
 
@@ -33,6 +40,18 @@ def auth_with_s3o(token):
     opener = urllib2.build_opener()
     opener.addheaders = [('FT-Authorization', token)]
     return opener.open(s3o_endpoint).getcode() == 204
+
+
+def lookupKeyInDynamo(ApiKeyId, token):
+    client = boto3.resource('dynamodb')
+    table = client.Table('lantern_api_gateway_key_store')
+    item = table.get_item(Key={'apiId':ApiKeyId}, AttributesToGet=['apiKey'])
+
+    if "Item" in item:
+        key = item['Item']['apiKey']
+        return token == key
+    print ('invalid api key')
+    return False
 
 
 class HttpVerb:
--- tests/test_authoriser.py
+++ tests/test_authoriser.py
@@ -1,5 +1,38 @@
 from nose.tools import assert_equals
+import mock, unittest, authoriser
+from mock import Mock
 
 
-def test_realtime_template():
-    assert_equals(1, 1)
+class AuthoriserTest(unittest.TestCase):
+
+    @mock.patch('authoriser.urllib2.build_opener', autospec=True)
+    def test_auth_with_s3o(self, mock_urllib2):
+
+        code = Mock()
+        code.getcode.side_effect = [204, 403]
+        resp = Mock()
+        resp.open.return_value = code
+        mock_urllib2.return_value = resp
+
+        assert_equals(authoriser.auth_with_s3o('123'), True)
+        resp.open.assert_called_with('https://s3o.ft.com/token/validate')
+
+        assert_equals(authoriser.auth_with_s3o('123'), False)
+
+    @mock.patch('authoriser.boto3.resource', autospec=True)
+    def test_look_up_key_in_dynamo(self, mock_boto3):
+
+        table = Mock()
+        resource = Mock()
+        resource.Table.return_value = table
+        table.get_item.side_effect = [{"Item": {"apiKey": "abc"}}, {"Item": {"apiKey": "abcd"}}]
+        mock_boto3.return_value = resource
+
+        assert_equals(authoriser.lookupKeyInDynamo('123', 'abc'), True)
+        table.get_item.assert_called_with(AttributesToGet=['apiKey'], Key={'apiId': '123'})
+
+        assert_equals(authoriser.lookupKeyInDynamo('123', 'abc'), False)
+
+
+
+
