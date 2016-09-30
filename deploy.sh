5378e206d7644fd2f27b534d9397e61109161344
ola.okejimi <ola.okejimi@ft.com>
2016-09-30 09:14:26 +0100

    Move aws policy class to another file

--- deploy.sh
+++ deploy.sh
@@ -1,2 +1,2 @@
-zip authoriser.zip authoriser.py
+zip authoriser.zip authoriser.py awspolicy.py
 aws lambda update-function-code --zip-file fileb://authoriser.zip --function-name s3o-api-gateway-custom-authoriser --publish --region eu-west-1
